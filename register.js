3e2a4d8ae126878d3e12e2ebe6acbbb245fa0454
TeddlySS <chonlada.bunw@gmail.com>
2025-11-19 15:23:17 +0700

    Update register.js

--- register.js
+++ register.js
@@ -274,7 +274,7 @@ async function signInWithGoogleFromRegister() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: 'https://strong-torrone-891034.netlify.app/',
+        redirectTo: 'https://strong-torrone-891034.netlify.app/challenge.html',
       },
     });
 
